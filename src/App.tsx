4b7090a38f54a9cc9533c7168b004fcf2c8ebb97
Bruno Nunes <bruno.nunes72@hotmail.com>
2024-01-30 21:53:14 -0300

    feat: started dev environment

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { Box, Button, Heading, Text, Flex } from "@chakra-ui/react";
+// import { Icon } from "@iconify/react";
+import React from "react";
+
+function App() {
+    // const [count, setCount] = React.useState(0);
+
+    
+    
+
+    return (
+      <>
+      </>
+    );
+}
+
+export default App;
